72a2a8b1c11407a97a716fc21cf2789f86e068c9
Ashish Pisey <ashish.pisey@peopleinteractive.in>
2015-01-29 11:03:20 +0530

    Initial Commit

---
+++ swiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftDemo
+//
+//  Created by Shaadi_mac1 on 29/01/15.
+//  Copyright (c) 2015 Ashish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
